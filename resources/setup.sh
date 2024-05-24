6d6284c03af04f30213de619b1c54038321e4fe8
Ravisankar Pandian <ravisankar.pandian@programmers.io>
2024-05-24 19:36:13 +0530

    update

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,11 +1,45 @@
-# bash/zsh git prompt support
+#!/QOpenSys/usr/bin/bash
+# ------------------------------------------------------------------------- #
+# Program       : setup.sh
+# Author        : Ravisankar Pandian
+# Company       : Programmers.io
+# Date Written  : 24/05/2024
+# Copyright     : Programmers.io
+# ------------------------------------------------------------------------- #
+
+progress_bar() {
+    # $1 is the total work
+    # $2 is the work done so far
+    local total_work=$1
+    local work_done=$2
+    local progress=$((work_done*20/total_work))  # 20 because 100/5=20
+    local filled_part=$(printf "%${progress}s" "")
+    local empty_part=$(printf "%$((20-progress))s" "")  # 20 because 100/5=20
+    printf "|%s%s| %s%%\r" "${filled_part// /#}" "${empty_part}" "$((work_done*100/total_work))"
+}
+
+show_progress(){
+  total_work=$1
+  echo "doing it at $1 %"
+  for i in $(seq 1 $total_work); do
+      # Simulate some work with sleep
+      sleep 0.005
+      progress_bar $total_work $i
+  done
+  echo ""  # Newline after progress bar
+
+}
+
+
 
 # Set bash as the default shell.
 /QOpenSys/pkgs/bin/chsh -s /QOpenSys/pkgs/bin/bash $USER
+echo "Changed the default shell to bash..."
 
 # create a .profile file in your home folder to store the environment variables.
+echo "Setup the environment variables..."
+show_progress 5
 cd ~
-touch .profile
 echo "export PATH=/QOpenSys/pkgs/bin:$PATH" >> .profile
 echo "export JAVA_HOME=/QOpenSys/QIBM/ProdData/JavaVM/jdk17/64bit" >> .profile
 echo "export JENKINS_HOME=/home/$USER/jenkins" >> .profile
@@ -15,15 +49,18 @@ echo "source ~/.gitprompt.sh" >> .profile
 ###############-------------- >>> Need to add the PS1 variable change here
 
 # Download the Git Prompt from GitHub
-wget https://github.com/ravisankar-PIO/gitonibmi/blob/main/gitprompt.sh -o .gitprompt.sh
+echo "Download the Git Promt from GitHub..."
+wget -v https://github.com/ravisankar-PIO/gitonibmi/blob/main/gitprompt.sh -o .gitprompt.sh
 
 
 # Update and upgrade the YUM
-yum update -y && yum upgrade -y
+echo "update the yum repository and existing packages..."
+yum -v update -y  && yum -v upgrade -y 
 
 
 # Install GIT
-yum install git -y
+echo "Install GIT..."
+yum -v install git -y
 git config --global user.name 'Ravisankar Pandian' 
 git config --global user.email ravisankar.pandian@programmers.io
 ssh-keygen -t ed25519 -C "ravisankar.pandian@programmers.io"
@@ -31,8 +68,10 @@ ssh-keygen -t ed25519 -C "ravisankar.pandian@programmers.io"
 ##############################################################################
 
 # Install Jenkins
-mkdir ~/jenkins
-wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war -o ~/jenkins/jenkins.war
+mkdir ~/jenkinss
+echo "Download Jenkins..."
+wget -v http://mirrors.jenkins.io/war-stable/latest/jenkins.war -o ~/jenkinss/jenkins.war
 
 # Install Service Commander
-yum install service-commander -y
+echo "Install Service Commander..."
+yum -v install service-commander -y
